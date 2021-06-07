6e04e438c5266f06a6e69c8c3b1fac5ef8bee406
ivyqiuhan <he0qiuhan@gmail.com>
2021-06-06 19:17:07 -0700

    initial setup for react and spring boot

---
+++ src/main/java/com/group/CSStudentJobSearchApp/CsStudentJobSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.group.CSStudentJobSearchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsStudentJobSearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsStudentJobSearchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/CSStudentJobSearchApp/CsStudentJobSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.CSStudentJobSearchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsStudentJobSearchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
