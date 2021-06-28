29a1b5d220d2faab35efe02a5d7d22c8001d086b
ivyqiuhan <he0qiuhan@gmail.com>
2021-06-27 19:43:25 -0700

    deleted user profile class

--- src/main/java/com/group/CSStudentJobSearchApp/model/User.java
+++ src/main/java/com/group/CSStudentJobSearchApp/model/User.java
@@ -6,11 +6,19 @@ import java.util.UUID;
 public class User {
     private final UUID id;
     private final String email;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String location;
     private Date lastLogin;
 
-    public User(UUID id, String email, Date lastLogin) {
+    public User(UUID id, String email, String password, String name, String phoneNumber, String location, Date lastLogin) {
         this.id = id;
         this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.location = location;
         this.lastLogin = lastLogin;
     }
 
@@ -27,8 +35,44 @@ public class User {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
     public Date getLastLogin() {
         return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
     }
 }
 
