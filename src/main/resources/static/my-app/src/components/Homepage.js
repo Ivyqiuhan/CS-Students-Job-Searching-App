af8707b2a0f0120aee6f3a8a41c28d891cd0aeff
Sherry <bcereal@students.cs.ubc.ca>
2021-07-05 17:40:19 -0700

    Started on dropdown for queries

--- src/main/resources/static/my-app/src/components/Homepage.js
+++ src/main/resources/static/my-app/src/components/Homepage.js
@@ -1,10 +1,32 @@
 import "./Homepage.css";
 import React from "react";
+import Dropdown from './dropdown/Dropdown';
+
+//testing
+const items = [
+  {
+    id: 1,
+    value: 'One'
+  },
+  {
+    id: 2,
+    value: 'Two'
+  },
+  {
+    id: 3,
+    value: 'Three'
+  }
+]
 
 const Home = () => {
   return (
     <div className="Homepage">
       Hello World
+
+      <div className="container">
+        <Dropdown title="Select..." items={items}/> 
+      </div>
+      
     </div>
   );
 }
