b2acaa1a1e478eee08308c2644219a8f778ba643
andreasy <ac72177@yahoo.com>
2021-07-27 20:51:34 -0700

    differentiated between registered and anonymous search

--- src/main/resources/static/my-app/src/components/queries/RegisteredSearch.js
+++ src/main/resources/static/my-app/src/components/queries/RegisteredSearch.js
@@ -14,62 +14,70 @@ const companyItems = [
 const leverLocationItems = [
     {id: 0, value: 'Remote'},
     {id: 1, value: 'Flexible'},
-    // {id: 2, value: 'Chicago'},
+    {id: 2, value: 'Chicago'},
     {id: 3, value: 'Los Angeles'},
-    // {id: 4, value: 'San Francisco'},
+    {id: 4, value: 'San Francisco'},
     {id: 5, value: 'Toronto'}
 ]
 
 const netflixLocationItems = [
     {id: 0, value: 'Remote, United States'},
-    // {id: 1, value: 'Alphaville, Brazil'},
+    {id: 1, value: 'Alphaville, Brazil'},
     {id: 2, value: 'Amsterdam, Netherlands'},
-    // {id: 3, value: 'Bangkok, Thailand'},
-    // {id: 4, value: 'Berlin, Germany'},
-    // {id: 5, value: 'Bogota, Columbia'},
-    // {id: 6, value: 'Buenos Aires, Argentina'},
-    // {id: 7, value: 'Copenhagen, Denmark'},
-    // {id: 8, value: 'Istanbul, Turkey'},
+    {id: 3, value: 'Bangkok, Thailand'},
+    {id: 4, value: 'Berlin, Germany'},
+    {id: 5, value: 'Bogota, Columbia'},
+    {id: 6, value: 'Buenos Aires, Argentina'},
+    {id: 7, value: 'Copenhagen, Denmark'},
+    {id: 8, value: 'Istanbul, Turkey'},
     {id: 9, value: 'London, United Kingdom'},
     {id: 10, value: 'Los Angeles, California'},
     {id: 11, value: 'Los Gatos, California'},
-    // {id: 12, value: 'Madrid, Spain'},
-    // {id: 13, value: 'Manila, Philippines'},
+    {id: 12, value: 'Madrid, Spain'},
+    {id: 13, value: 'Manila, Philippines'},
     {id: 14, value: 'Mexico City, Mexico'},
     {id: 15, value: 'Mumbai, India'},
-    // {id: 16, value: 'New York, New York'},
-    // {id: 18, value: 'Oregon'},
-    // {id: 19, value: 'Paris, France'},
-    // {id: 20, value: 'Rio de Janeiro'},
-    // {id: 21, value: 'Salt Lake City, Utah'},
+    {id: 16, value: 'New York, New York'},
+    {id: 18, value: 'Oregon'},
+    {id: 19, value: 'Paris, France'},
+    {id: 20, value: 'Rio de Janeiro'},
+    {id: 21, value: 'Salt Lake City, Utah'},
     {id: 22, value: 'Seoul, South Korea'},
     {id: 23, value: 'Singapore, Singapore'},
-    // {id: 24, value: 'Stockholm, Sweden'},
-    // {id: 25, value: 'Sydney, Australia'},
+    {id: 24, value: 'Stockholm, Sweden'},
+    {id: 25, value: 'Sydney, Australia'},
     {id: 26, value: 'Tokyo, Japan'},
-    // {id: 27, value: 'Toronto, Canada'}
+    {id: 27, value: 'Toronto, Canada'}
 ]
 
-class AnonymousSearch extends React.Component {
+const leverCommitmentItems = [
+    {id: 0, value: 'Full-time'}
+]
+
+const netflixCommitmentItems = [
+    {id: 0, value: 'A/B Test'},
+    {id: 1, value: 'Contractor'},
+    {id: 2, value: 'Chicago'},
+    {id: 3, value: 'Full-time'},
+    {id: 4, value: 'Pipeline'},
+    {id: 5, value: 'Uncategorized'}
+]
+
+class RegisteredSearch extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            locationValue: '',
             companyValue: '',
+            locationValue: '',
+            commitmentValue: '',
             viewQueries: false,
             companySelected: false
         };
 
         this.onChangeLocation = this.onChangeLocation.bind(this);
-    }
+        this.onChangeCommitment = this.onChangeCommitment.bind(this);
 
-    onChangeLocation = (newLocation) => {
-        this.setState({
-            locationValue: newLocation.value
-        }, () => {
-            console.log('new state', this.state.locationValue);
-        });
     }
 
     onChangeCompany = (newCompany) => {
@@ -80,7 +88,23 @@ class AnonymousSearch extends React.Component {
         });
     }
 
-    makeLeverRequest = (jobArray) => {
+    onChangeLocation = (newLocation) => {
+        this.setState({
+            locationValue: newLocation.value
+        }, () => {
+            console.log('new state', this.state.locationValue);
+        });
+    }
+
+    onChangeCommitment = (newCommitment) => {
+        this.setState({
+            commitmentValue: newCommitment.value
+        }, () => {
+            console.log('new commitment', this.state.commitmentValue);
+        });
+    }
+
+    makeLeverAPIRequest = (jobArray) => {
         for (let job of jobArray) {
             let data = {
                 jobKey: 0,
@@ -101,12 +125,13 @@ class AnonymousSearch extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        axios.get('https://api.lever.co/v0/postings/' + this.state.companyValue + '?skip=0&limit=25&mode=json&location=' +
+// lever attempt
+        axios.get('https://api.lever.co/v0/postings/' + this.state.companyValue + '?skip=0&limit=50&mode=json&location=' +
             this.state.locationValue
         )
             .then(res => {
                 console.log(res.data);
-                this.makeLeverRequest(res.data);
+                this.makeLeverAPIRequest(res.data);
             });
 
         this.setState({viewQueries: true});
@@ -138,6 +163,16 @@ class AnonymousSearch extends React.Component {
                                 changeValue={this.onChangeLocation}/>
                         </div>
                     </div>
+                    <div className="dd-container">
+                        <div className="container">
+                            <Dropdown
+                                title="Commitment"
+                                items= {(this.state.companyValue == 'netflix') ? netflixCommitmentItems :
+                                    (this.state.companyValue == 'lever') ? leverCommitmentItems :
+                                        [{id: 0, value: 'Select a company first'}]}
+                                changeValue={this.onChangeCommitment}/>
+                        </div>
+                    </div>
 
                     <div className="button-container">
                         <button onClick={this.handleSubmit} className="submit-button"> Submit</button>
@@ -150,4 +185,5 @@ class AnonymousSearch extends React.Component {
     }
 }
 
-export default AnonymousSearch
+
+export default RegisteredSearch
